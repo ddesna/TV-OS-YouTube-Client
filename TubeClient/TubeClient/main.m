23fb0554bb7e7fa5b2b14f521b44d7ef7623652d
Danny <Dani@MacBook-Pro-de-Daniil.local>
2015-10-10 22:50:57 +0200

    Initial commit

---
+++ TubeClient/TubeClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TubeClient
+//
+//  Created by Danny on 10/10/15.
+//  Copyright © 2015 DDE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
